d2c256a230dfde69542a6cf837eb569e731b5ea2
picuu <carloscapoforcada@gmail.com>
2024-05-20 22:07:06 +0200

    create section layout

---
+++ web/src/sections/LayoutSection.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "LayoutSection"
+}
+</script>
+
+<template>
+  <section>
+    <slot />
+  </section>
+</template>
+
+<style scoped>
+section {
+  padding-bottom: var(--section-gap);
+}
+</style>
---
+++ web/src/sections/SectionHero.vue
@@ -0,0 +1,16 @@
+<script>
+import LayoutSection from '@/sections/LayoutSection.vue'
+
+export default {
+  name: "SectionHero",
+  components: {
+    LayoutSection,
+  }
+}
+</script>
+
+<template>
+  <LayoutSection>
+    <h1>HERO</h1>
+  </LayoutSection>
+</template>
