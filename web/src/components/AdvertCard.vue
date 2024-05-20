cadb141aee1ca1f37ed8eb43462651913812cedb
picuu <carloscapoforcada@gmail.com>
2024-05-20 22:07:31 +0200

    add AdvertCard and CardsRow components

---
+++ web/src/components/AdvertCard.vue
@@ -0,0 +1,34 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  name: "AdvertCard",
+  props: {
+    title: String,
+    description: String,
+    image: String,
+    link: String
+  },
+  components: {
+    RouterLink
+  }
+}
+</script>
+
+<template>
+  <article>
+    <img :src="image" :alt="title">
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+    <RouterLink :to="link">See more</RouterLink>
+  </article>
+</template>
+
+<style scoped>
+article {
+  padding: 1rem;
+  background-color: rgb(100 100 100 / .15);
+  border: 1px solid rgb(100 100 100 / .25);
+  border-radius: .5rem;
+}
+</style>
---
+++ web/src/components/CardsRow.vue
@@ -0,0 +1,26 @@
+<script>
+import AdvertCard from "@/components/AdvertCard.vue"
+
+export default {
+  name: "CardsRow",
+  components: {
+    AdvertCard
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AdvertCard title="Offers" link="/offers" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis efficitur mauris et laoreet. Nulla consequat sed mauris non ornare. Morbi molestie dui non laoreet varius." />
+    <AdvertCard title="Demands" link="/demands" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis efficitur mauris et laoreet. Nulla consequat sed mauris non ornare. Morbi molestie dui non laoreet varius." />
+    <AdvertCard title="Events" link="/events" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis efficitur mauris et laoreet. Nulla consequat sed mauris non ornare. Morbi molestie dui non laoreet varius." />
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1.5rem;
+}
+</style>
