670f70043ff800545e6ac5cd238601cbbac9bee6
picuu <carloscapoforcada@gmail.com>
2024-05-23 10:55:21 +0200

    update CardsRow and SectionHero components

--- web/src/components/CardsRow.vue
+++ web/src/components/CardsRow.vue
@@ -1,39 +1,39 @@
 <script>
-import AdvertCard from "@/components/AdvertCard.vue"
-import { IconHeartHandshake, IconMessageQuestion, IconFriends } from '@tabler/icons-vue';
+import AdvertCard from '@/components/AdvertCard.vue'
+import LayoutSection from '@/sections/LayoutSection.vue'
+import { IconHeartHandshake, IconMessageQuestion } from '@tabler/icons-vue'
 
 export default {
-  name: "CardsRow",
+  name: 'CardsRow',
   components: {
     AdvertCard,
+    LayoutSection,
     IconHeartHandshake,
-    IconMessageQuestion,
-    IconFriends
+    IconMessageQuestion
   }
 }
 </script>
 
 <template>
-  <div>
-    <AdvertCard title="Offers" link="/offers" description="See what others have to offer you">
-      <IconHeartHandshake class="icon" size="92" stroke="1" />
-    </AdvertCard>
+  <LayoutSection>
+    <div>
+      <AdvertCard title="Offers" link="/offers" description="See what others have to offer you">
+        <IconHeartHandshake class="icon" size="92" stroke="1" />
+      </AdvertCard>
 
-    <AdvertCard title="Requests" link="/demands" description="Ask others for a specific service">
-      <IconMessageQuestion class="icon" size="92" stroke="1" />
-    </AdvertCard>
-
-    <AdvertCard title="Workshops" link="/events" description="Learn and teach together, without limits">
-      <IconFriends class="icon" size="92" stroke="1" />
-    </AdvertCard>
-  </div>
+      <AdvertCard title="Requests" link="/demands" description="Ask others for a specific service">
+        <IconMessageQuestion class="icon" size="92" stroke="1" />
+      </AdvertCard>
+    </div>
+  </LayoutSection>
 </template>
 
 <style scoped>
 div {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   justify-items: center;
+  width: calc(100% - 300px);
+  margin-inline: auto;
 }
 </style>
-
