6c668550fb192b6d602531c630d72f74db758208
picuu <carloscapoforcada@gmail.com>
2024-05-23 10:54:02 +0200

    fix background size

--- web/src/components/BackgroundComponent.vue
+++ web/src/components/BackgroundComponent.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  name: "BackgroundComponent"
+  name: 'BackgroundComponent'
 }
 </script>
 
@@ -14,8 +14,8 @@ div {
   top: 0;
   left: 0;
   z-index: -2;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   background-color: rgb(10 10 10);
   background: radial-gradient(ellipse 80% 80% at 50% -20%, rgba(120 119 198 / 0.3), rgba(255 255 255 / 0));
 }
