f5d7145c2eaad31585cb5c463039b139b7a8fcad
picuu <carloscapoforcada@gmail.com>
2024-05-23 13:57:31 +0200

    update background color

--- web/src/components/BackgroundComponent.vue
+++ web/src/components/BackgroundComponent.vue
@@ -17,6 +17,6 @@ div {
   height: 100%;
   width: 100%;
   background-color: rgb(10 10 10);
-  background: radial-gradient(ellipse 80% 80% at 50% -20%, rgba(120 119 198 / 0.3), rgba(255 255 255 / 0));
+  background: radial-gradient(ellipse 80% 80% at 50% -20%, rgba(72, 89, 196, 0.3), rgba(255 255 255 / 0));
 }
 </style>
