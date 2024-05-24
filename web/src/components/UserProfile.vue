958212de727b5a7aac775168479363cdaebed50a
picuu <carloscapoforcada@gmail.com>
2024-05-24 13:57:59 +0200

    add user profile view

---
+++ web/src/components/UserProfile.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "UserProfile",
+  props: {
+    userId: { type: String, default: "0" }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+
+</style>
--- web/src/views/UserProfileView.vue
+++ web/src/views/UserProfileView.vue
@@ -1,14 +1,28 @@
 <script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+import UserProfile from '../components/UserProfile.vue';
+import LayoutSection from '../sections/LayoutSection.vue';
 export default {
   name: "LoginView",
   props: {
     userId: String
-  }
+  },
+  components: { HeaderComponent, LayoutSection, UserProfile }
 };
 </script>
 
 <template>
-  
+  <div>
+    <HeaderComponent />
+
+    <main class="content-grid">
+      <LayoutSection>
+        <h2>User profile</h2>
+
+        <UserProfile :userId="userId" />
+      </LayoutSection>
+    </main>
+  </div>
 </template>
 
 <style scoped>
