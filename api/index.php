7b65be207830fbf39f600734b32244c11a5095a2
jacomar <jacomar304@hotmail.com>
2024-05-28 01:32:01 +0200

    Changes to the 'LoginForm.vue' component

--- api/index.php
+++ api/index.php
@@ -26,6 +26,7 @@
     $uri = explode('/', $uri);
     $numElementsRuta = count($uri); // echo $numElementsRuta."<br>";
 
+    // Configuración de CORS
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
