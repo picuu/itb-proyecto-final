5ef3b994c5959aace824a8d3d64d12ba4aaf0b5a
picuu <carloscapoforcada@gmail.com>
2024-05-28 12:11:07 +0200

    refactor validateSession helper to return loggin info if user is logged

--- web/src/helpers/validateSession.js
+++ web/src/helpers/validateSession.js
@@ -1,4 +1,4 @@
 export function validateSession() {
-  if (localStorage.getItem("authInfo")) return true
+  if (localStorage.getItem("authInfo")) return JSON.parse(localStorage.getItem("authInfo"))
   return false
 }
