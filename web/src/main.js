5d73f721b0097eecafe284615a439483ad2b0f73
picuu <carloscapoforcada@gmail.com>
2024-05-30 23:23:58 +0200

    remove unnecesary dependences

--- web/src/main.js
+++ web/src/main.js
@@ -4,15 +4,8 @@ import '@fontsource-variable/onest'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VCalendar from 'v-calendar'
-import 'v-calendar/style.css'
-import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
-import 'v-calendar/style.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VCalendar, {})
-app.component('VCalendar', Calendar)
-app.component('VDatePicker', DatePicker)
 app.mount('#app')
